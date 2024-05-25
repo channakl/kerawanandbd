3cf7f4205a3e2ca499a98fba9efebf19eb25c2f6
channakl <sunturday10@gmail.com>
2024-05-25 20:07:58 +0700

    adjust map default zoom level

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -6,7 +6,7 @@ import "leaflet-defaulticon-compatibility";
 const Map = (props) => {
     const { children, geoJson, geoJsonStyle, onEachGeoJsonFeature = () => {} } = props;
     return (
-        <MapContainer zoom={16} center={[-7.29705665224, 112.770197762]} scrollWheelZoom style={{ width: '100%', height: '100%', borderRadius: '.5rem'}}>
+        <MapContainer zoom={15} center={[-7.29705665224, 112.770197762]} scrollWheelZoom style={{ width: '100%', height: '100%', borderRadius: '.5rem'}}>
             <TileLayer
             attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url={`https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=${process.env.STADIAMAPS_API_KEY}`}
